17373e8ff382482366cd1dafbf12da0b44dd9692
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2023-03-19 20:46:56 +0200

    Showed lessons information

--- course/course.js
+++ course/course.js
@@ -6,7 +6,43 @@ async function getData() {
 
 async function main() {
     const data = await getData();
-    console.log(data);
-}
+    let lessons = data.lessons;
 
+    function displayCourse(data) {
+        const myCourseHeader = document.querySelector('.my-course-header');
+        myCourseHeader.innerHTML = `
+            <div class="course-image">
+                <img src="${data.previewImageLink}/cover.webp" alt="${data.title}">
+            </div>
+            <div class="course-info">
+                <h3>${data.title}</h3>
+                <p class="course-description">${data.description}</p>
+                <p class="course-rating">&#9733; ${data.rating}</p>
+            </div>
+        `
+    }
+
+    function displayLessons(arrData) {
+        let lock = '';
+        arrData.forEach(el => {
+            if (el.status == 'locked') lock = '&#128274;';
+
+            const lessonsList = document.querySelector('.my-course-lessons');
+            console.log(el);
+            lessonsList.innerHTML += `
+                <div class="lesson">
+                    <div class="lesson-image">
+                        <img src="${el.previewImageLink}/lesson-${el.order}.webp" alt="video${el.order}">
+                    </div>
+                    <div class="lesson-title">
+                        <h4>${el.order}.&nbsp;${el.title}&nbsp;${lock}</h4>
+                    </div>
+                </div>
+            `
+        });
+    }
+
+    displayCourse(data);
+    displayLessons(lessons);
+}
 main();
